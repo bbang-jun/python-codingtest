51d00864449776259359cc11af3ab4bcfb01956e
BYEONGJUN KANG <90714030+bbang-jun@users.noreply.github.com>
2023-06-28 18:07:45 +0900

    [Bronze III] Title: 곱셈, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,19 @@
+a = input()
+b = input()
+str0 = b[0]
+str1 = b[1]
+str2 = b[2]
+
+a = int(a)
+b = int(b)
+temp = int(str2)
+
+print(a*temp)
+
+temp = int(str1)
+print(a*temp)
+
+temp = int(str0)
+print(a*temp)
+
+print(a*b)
